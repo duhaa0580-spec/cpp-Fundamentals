6efb0862d1285b925918505a3a2532252998f092
Duha Ahmed <daa464464@gmail.com>
2026-08-03 15:43:09 +0300

    first commit

---
+++ test.cpp
@@ -0,0 +1,225 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+    // App 1 => Even / Odd Checker
+  //  int num;
+  // cout << " Enter your Number " << endl;
+  // cin >> num;
+    
+  //  if (num % 2 ==0) {
+ //       cout << " The Number Is Even "  << endl;
+ //  } else {
+  //      cout << " The Number Is Odd "  << endl;
+  //  }
+ // App 2 => Find Greatest Number 
+
+ // int x,y,z;
+ // cout << " Enter your Number " << endl;
+ // cin >> x;
+ // cin >> y;
+ // cin >> z;
+
+ // if ( x > y && x > z) {
+ //   cout <<  x << " Is The Greatest Number " << endl;
+ // } else if (y > z) {
+ //   cout << y << " Is The Greatest Number " << endl;
+ // } else {
+ //   cout << z << " Is The Greatest Number " << endl;
+ // }
+ // App 3 => User Rank Checker 
+
+ //int points;
+ // cout << " Enter your points " << endl;
+ // cin >> points;
+
+ //if (  points > 0 && points <= 500  ) {
+ //   cout << " Bad  " << endl;
+ //} else if ( points > 500 && points <= 1000 ) {
+ //   cout << " Very Good " << endl;
+ //} else {
+ //   cout << " you are VIP Bro  " << endl;
+ //} 
+ // App 4 => Simple Calculator 
+
+ // int num_one, num_two, op;
+ // cout << " Enter num_one" << endl;
+ //cin >> num_one;
+ //cout << " Enter num_two" << endl;
+ //cin >> num_two;
+ //cout <<" Choose Operations " << endl;
+ //cout << "[1] + " << endl;
+ //cout << "[2] - " << endl;
+ //cout << "[3] * " << endl;
+ //cout << "[4] / " << endl;
+ //cin >> op;
+
+ //if ( op == 1) {
+ // cout << num_one + num_two << endl;
+ //} else if ( op == 2) {
+ // cout <<  num_one - num_two  << endl;
+ //} else if ( op == 3) {
+ // cout <<  num_one * num_two  << endl;
+ //} else if ( op == 4) {
+ // cout <<  num_one / num_two  << endl;
+ //} else {
+ // cout << " Operation Is Not Valid " << endl;
+ //}
+
+ //int day;
+ //cout << " Choose A Days From 1 To 25 " << endl;
+ //cin >> day;
+
+ //if ( day == 1) {
+ // cout << " Open From 08:00 To 14:00 " << endl;
+ //} else if ( day == 2) {
+ // cout << " Open From 08:00 To 14:00 " << endl;
+ //} else if ( day == 3) {
+ // cout <<" Open From 15:00 To 20 " << endl;
+ //} else {
+  //  cout << " closed " << endl;
+ // }
+
+  //switch (day) {
+ // case 1:
+  //case 2:
+ // cout << "Open From 08:00 To 14:00 " << endl;
+ // break;
+ // case 3:
+ // cout << "Open From 10:00 To 20:00 " << endl;
+ // break;
+ // default: 
+ //    cout << " closed " << endl;
+ //}
+
+  // Switch Training //
+  // App 1 => Award System Application
+
+ // int num;
+ // cout << "Type The Number" << endl;
+ //cin >> num;
+
+ //switch (num) {
+ // case 100:
+ //cout <<" Congraulations , You can take sumsung phone " << endl;
+ // break;
+ // case 200:
+ //cout << " Congraulations , You can take Lenovo Laptop " << endl;
+ // break;
+ // case 400:
+ //cout << " Congraulations ,You can take TV " << endl;
+ // break;
+ //default:
+ // cout << " Sorry , Your number is not correct  " << endl;
+ 
+ //}
+
+ // App 2 => Discount App
+ //int price = 100;
+ //int discount = 10;
+ //int years;
+
+ //cout <<" Type The Number Of Years in Company " << endl;
+ //cin >> years;
+
+ //switch (years) {
+  //case 1:
+ //discount = 20;
+ // break;
+ // case 2:
+ //discount = 40;
+ // break;
+ // case 3:
+ //discount = 60;
+ // break;
+
+ //}
+ // cout << " The Price Is " << price - discount << endl;
+ 
+ // App 3 Simple Calculator 
+
+  //int n1 , n2 , op;
+ 
+ //cout << " Enter n1 " << endl;
+ //cin >> n1;
+ //cout << " Enter n2 " << endl;
+ //cin >> n2;
+ //cout <<" Choose Operations " << endl;
+ //cout << "[1] + " << endl;
+ //cout << "[2] - " << endl;
+ //cout << "[3] * " << endl;
+ //cout << "[4] / " << endl;
+ //cin >> op;
+
+ //switch (op) {
+ // case 1:
+ // cout <<  n1 << " + " <<  n2  << " = " << n1 + n2 << endl;
+ //break;
+ //case 2:
+ // cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+ //break;
+ //case 3:
+ // cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+ // break;
+ //case 4:
+ // cout << n1 << " / " << n2 << " = " << n1 / n2 << endl;
+ //break;
+ //default:
+ // cout << " Operation Is Not Valid " << endl;
+ //}
+
+
+ //int nums[]{100, 300, 600, 900};
+ //string awards[]{"iPhone", "iPad", "PC", "Car"};
+
+    // cout << " / Number " << num[0] << "Award Is " << awards[0] << "\"" << endl;
+   // cout << " / Number " << num[1] << "Award Is " << awards[1] << "\"" << endl;
+    // cout << " / Number " << num[2] << "Award Is " << awards[2] << "\"" << endl;
+   // cout << " / Number " << num[3] << "Award Is " << awards[3] << "\"" << endl;
+   //     
+   // 
+
+    
+
+
+ // Output Needed
+ //"Number 100 Award Is: iPhone"
+ //"Number 300 Award Is: iPad"
+ // "Number 600 Award Is: PC"
+ //"Number 900 Award Is: Car"
+ // int prices [4] = {100, 200, 300, 400};
+ // cout << " The Price: " << prices [0] << endl;
+ // cout << " The Price: " << prices [1] << endl;
+   //cout << " The Price: " << prices [2] << endl;
+ //  cout << " The Price: " << prices [3] << endl;
+
+ //int scores [] = {95 ,90 ,85};
+ //cout << " The Score " << scores[0] << endl;
+ //cout << " The Score " << scores[1] << endl;
+ //cout << " The Score " << scores[2] << endl;
+ //cout << " Address 1: "<< &scores[0] << endl;
+ //cout << " Array Address: " << scores << endl;
+
+ //int scores [] = {100 , 200 ,400};
+ //cout << " The Score: " << scores[0] << endl;
+ ///cout << " The Score: " << scores[1] << endl;
+ //cout << " The Score: " << scores[2] << endl;
+ //cout << " Address 1: "<< &scores[0] << endl;
+ //cout << " Array Address: " << scores << endl;
+ //cout << " The Score: " << scores [2] << endl;
+ //cout << " ASCII of A is: " <<(int) 'A' << endl;
+ //cout << 'A' + 5;
+ cout << (char)('d' + 10);
+ 
+
+
+ return 0;
+
+
+
+
+
+
+
+
+}
